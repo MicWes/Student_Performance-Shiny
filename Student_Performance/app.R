8c1519a9305438b4b59c92e79d4a70ec78db0e02
Michael Wesley <micwesley@gmail.com>
2019-11-21 14:15:36 -0300

    plot malfeito

--- Student_Performance/app.R
+++ Student_Performance/app.R
@@ -14,25 +14,25 @@ library(RColorBrewer)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = "bootstrap.min.css",
-
-    # Application title
-    titlePanel("Student Performance"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+                
+                # Application title
+                titlePanel("Student Performance"),
+                
+                # Sidebar with a slider input for number of bins 
+                sidebarLayout(
+                    sidebarPanel(
+                        sliderInput("bins",
+                                    "Number of bins:",
+                                    min = 1,
+                                    max = 50,
+                                    value = 30)
+                    ),
+                    
+                    # Show a plot of the generated distribution
+                    mainPanel(
+                        plotOutput("distPlot")
+                    )
+                )
 )
 
 # Define server logic required to draw a histogram
@@ -40,31 +40,26 @@ server <- function(input, output) {
     d1=read.table("student-mat.csv",sep=";",header=TRUE)
     
     d2=read.table("student-por.csv",sep=";",header=TRUE)
-
+    
     d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
     
     #M <- cor(d3)
     #corrplot(M, type="upper", order="hclust", col=brewer.pal(n=8, name="RdYlBu"))
-
-    View(d3)
+    
+    #View(d1)
     summary(d3)
     
     sapply(d3,is.numeric)
     
     d3numeric <- d3[,sapply(d3, is.numeric)]
-    matriz_cor <- cor(d3numeric)
-    corrplot (matriz_cor, method="ellipse")
-    View(matriz_cor)
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$distPlot <- renderPlot({
+        matriz_cor <- cor(d3numeric)
+        corrplot(matriz_cor, method="ellipse")
+        View(matriz_cor, type="upper", order="hclust", col=brewer.pal(n=8, name="RdYlBu"))
     })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
